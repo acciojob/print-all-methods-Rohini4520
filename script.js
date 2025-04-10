18c76e55983c5270b5639a88a8026a43476f827a
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-04-10 12:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
-}
+	console.log(Math.abs, Math.acos, Math.acosh, Math.asin, Math.asinh, Math.atan, Math.atan2,
+    Math.atanh, Math.cbrt, Math.ceil, Math.clz32, Math.cos, Math.cosh, Math.exp,
+    Math.expm1, Math.floor, Math.fround, Math.hypot, Math.imul, Math.log, Math.log10,
+    Math.log1p, Math.log2, Math.max, Math.min, Math.pow, Math.random, Math.round,
+    Math.sign, Math.sin, Math.sinh, Math.sqrt, Math.tan, Math.tanh, Math.trunc);
 
+}
 alert(allMethods());
+
